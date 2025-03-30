4bd700bf73c450479a91abcfbec328082c820a87
Johnenis Silva Santos <johnenis@hotmail.com>
2025-03-29 21:49:37 -0300

    Comentarios

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+// Função para inserir conteudo na tela de display
 function insertToDisplay(data) {
   document.querySelector("#display").value += data;
 }
+// Função para limpar totalmente a tela
 function clean() {
   document.querySelector("#display").value = "";
 }
+// Função do Botão de Limpar um unico numero
 function back() {
   const display = document.querySelector("#display");
   display.value = display.value.slice(0, -1);
 }
+// Função logica de Calculo
 function result() {
   const display = document.querySelector("#display");
   try {
